e8c3b1d939038059ccedcd01ac8d0381e78ba998
Aryan <ap01n08@gmail.com>
2026-08-14 13:43:53 +0545

    chore(identity): Implementing identity on the application user

--- Presentation/Application/Identity/ApplicationUser.cs
+++ Presentation/Application/Identity/ApplicationUser.cs
@@ -5,7 +5,7 @@ namespace OrderManagement.Application.Identity;
 
 public class ApplicationUser : IdentityUser<Guid>
 {
-    public string FullName {get; set;} = string.Empty;
-    public TimestampAttribute CreatedAt {get; set;}
-    public bool IsActive {get; set;}
+    public string FullName { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public bool IsActive { get; set; }
 }
