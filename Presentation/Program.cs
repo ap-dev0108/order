596a060e25e735ee213f058b69a3a7a335e5b6c7
Aryan <ap01n08@gmail.com>
2026-08-16 21:03:57 +0545

    config(program): Made multiple changes. Added swagger gen. Refactoring Env Path. Adding policies for identity password.

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -5,20 +5,39 @@ using OrderManagement.Infrastructure.DI;
 using OrderManagement.Infrastructure.Load;
 using OrderManagement.Infrastructure.Persistence;
 using OrderManagement.Infrastructure.Seed;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddSingleton<EnvLoad>();
+
+var envPath = Path.Combine(
+    Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.Parent!.FullName,
+    "Infrastructure",
+    ".env"
+);
+Env.Load(envPath);
 
 var envLoad = new EnvLoad();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(envLoad.DbUrl));
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>()
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(opt =>
+{
+    opt.Password.RequireDigit = false;
+    opt.Password.RequiredLength = 4;
+    opt.Password.RequiredUniqueChars = 0;
+    opt.Password.RequireLowercase = false;
+    opt.Password.RequireNonAlphanumeric = false;
+    opt.Password.RequireUppercase = false;
+})
     .AddEntityFrameworkStores<AppDbContext>()
+    .AddSignInManager()
     .AddDefaultTokenProviders();
 
 RepoDI.RepoInjection(builder.Services);
@@ -29,11 +48,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 using (var scope = app.Services.CreateScope())
 {
-    await IdentitySeed.SeedData(scope.ServiceProvider);
+    var services = scope.ServiceProvider;
+    await IdentitySeed.SeedData(services);
 }
 
 app.UseHttpsRedirection();
