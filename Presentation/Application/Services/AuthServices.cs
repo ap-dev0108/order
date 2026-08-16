fee8f23bc5a5cd399ddc34bde618254c02d8f715
Aryan <ap01n08@gmail.com>
2026-08-16 21:47:09 +0545

    feat(register): Creating logics and validations for user registration

--- Presentation/Application/Services/AuthServices.cs
+++ Presentation/Application/Services/AuthServices.cs
@@ -33,4 +33,36 @@ public class AuthServices
         return "Login Done";
     }
 
+    public async Task<RegisterDTO> RegisterUser(RegisterDTO registerDTO)
+    {
+        var newUser = new ApplicationUser
+        {
+            FullName = registerDTO.Name,
+            Email = registerDTO.Email,
+            UserName = registerDTO.Name,
+            IsActive = true
+        };
+
+        var userExists = await _userManager.FindByEmailAsync(registerDTO.Email);
+
+        if (userExists != null)
+        {
+            throw new InvalidOperationException("User with this email already exists. Please login");
+        }
+
+        var userRegistration = await _userManager.CreateAsync(newUser, registerDTO.Password);
+
+        if (!userRegistration.Succeeded)
+        {
+            foreach (var error in userRegistration.Errors)
+            {
+                throw new Exception($"Error while registering user: {error.Description}");
+            }
+        }
+
+        await _userManager.AddToRoleAsync(newUser, "Kitchen");
+
+        return registerDTO;
+    }
+
 }
