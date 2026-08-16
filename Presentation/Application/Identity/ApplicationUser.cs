702fa829f3d23e9ad9a2cd1af7f503e9f125d358
Aryan <ap01n08@gmail.com>
2026-08-16 22:09:28 +0545

    update(user): Adding role column

--- Presentation/Application/Identity/ApplicationUser.cs
+++ Presentation/Application/Identity/ApplicationUser.cs
@@ -8,4 +8,5 @@ public class ApplicationUser : IdentityUser
     public string FullName { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public bool IsActive { get; set; }
+    public string Roles { get; set; } = string.Empty;
 }
