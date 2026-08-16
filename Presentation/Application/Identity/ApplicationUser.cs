78d07995db4ee1409a0ed2975e8945f40ca80a22
Aryan <ap01n08@gmail.com>
2026-08-16 21:02:21 +0545

    refactor(identity): Removing Guid as a necessary uuid

--- Presentation/Application/Identity/ApplicationUser.cs
+++ Presentation/Application/Identity/ApplicationUser.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace OrderManagement.Application.Identity;
 
-public class ApplicationUser : IdentityUser<Guid>
+public class ApplicationUser : IdentityUser
 {
     public string FullName { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
