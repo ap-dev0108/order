9119796de88fef0af2b40fd377cac018b8b6dfd9
Aryan <ap01n08@gmail.com>
2026-08-16 21:02:43 +0545

    chore(dto): Creating dto for login interface

---
+++ Presentation/Application/DTO/UserDTO.cs
@@ -0,0 +1,8 @@
+namespace OrderManagement.Application.DTO;
+
+public class UserDTO
+{
+    public string? FullName {get;set;}
+    public string? Email {get; set;}
+    public bool? isActive {get; set;}
+}
