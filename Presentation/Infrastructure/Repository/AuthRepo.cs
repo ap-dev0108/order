35812fcbcbaa8fbf5f494bb143571b491a2f2a03
Aryan <ap01n08@gmail.com>
2026-08-16 16:39:43 +0545

    fix(identity): Fixing IAuth repo methods

---
+++ Presentation/Infrastructure/Repository/AuthRepo.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using OrderManagement.Application.Identity;
+using OrderManagement.Application.Interface;
+
+namespace OrderManagement.Infrastructure.Repo;
+
+public class AuthRepo : IAuthRepo
+{
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AuthRepo(SignInManager<ApplicationUser> signInManager)
+    {
+        _signInManager = signInManager;
+    }
+
+    public async Task<SignInResult> LoginUser(ApplicationUser user)
+    {
+        return await _signInManager.CheckPasswordSignInAsync(user, user.PasswordHash, false);
+    }
+}
