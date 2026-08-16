0ddd6cc3de4fab62cc1b8e38856e393a33022044
Aryan <ap01n08@gmail.com>
2026-08-16 11:07:29 +0545

    chore(dependency): creating global injection file for services

---
+++ Presentation/Infrastructure/DI/ServicesDI.cs
@@ -0,0 +1,13 @@
+using OrderManagement.Application.Services;
+
+namespace OrderManagement.Infrastructure.DI;
+
+public class ServiceDI
+{
+    public static IServiceCollection ServiceInjection(IServiceCollection services)
+    {
+        services.AddScoped<AuthServices>();
+
+        return services;
+    }
+}
