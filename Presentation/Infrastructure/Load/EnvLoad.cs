78af002a5ca13192528211bc1a0baf8d3c364cb8
Aryan <ap01n08@gmail.com>
2026-08-16 11:01:14 +0545

    chore(env): Adding admin email and password for env

--- Presentation/Infrastructure/Load/EnvLoad.cs
+++ Presentation/Infrastructure/Load/EnvLoad.cs
@@ -3,10 +3,14 @@ namespace OrderManagement.Infrastructure.Load;
 public class EnvLoad
 {
     public string DbUrl { get; set; } = string.Empty;
+    public string AdminEmail {get; set;} = string.Empty;
+    public string AdminPassword {get; set;} = string.Empty;
 
     public EnvLoad()
     {
         DbUrl = GetRequired("DATABASE_URL");
+        AdminEmail = GetRequired("ADMIN_EMAIL");
+        AdminPassword = GetRequired("ADMIN_PASSWORD");
     }
 
     public static string GetRequired(string key)
