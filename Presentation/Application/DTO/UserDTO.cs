67832381e370f31ecb2ece1a43707ee477fc72ad
Aryan <ap01n08@gmail.com>
2026-08-20 13:35:06 +0545

    refactor(dto): updating user dto with id

--- Presentation/Application/DTO/UserDTO.cs
+++ Presentation/Application/DTO/UserDTO.cs
@@ -2,6 +2,7 @@ namespace OrderManagement.Application.DTO;
 
 public class UserDTO
 {
+    public string Id {get; set;}
     public string? FullName {get;set;}
     public string? Email {get; set;}
     public bool? isActive {get; set;}
