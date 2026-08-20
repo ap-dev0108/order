9add8031c2133eb6832b94c49c63d379959d1d4a
Aryan <ap01n08@gmail.com>
2026-08-20 15:26:02 +0545

    feat(api): Creating feature for product operations

---
+++ Presentation/Api/Controller/ProductController.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.Mvc;
+using OrderManagement.Application.DTO;
+using OrderManagement.Application.DTO.Products;
+using OrderManagement.Application.Services;
+
+namespace OrderManagement.Api.Controller;
+
+public class ProductController : ControllerBase
+{
+    private readonly ProductServices _product;
+
+    public ProductController(ProductServices product)
+    {
+        _product = product;
+    }
+
+    [HttpGet("all")]
+    public async Task<IActionResult> GetProductsList()
+    {
+        var productList = await _product.GetProductsAsync();
+
+        return Ok(new Response<List<ProductDTO>>
+        {
+            Success = true,
+            Message = "Product List fetched",
+            Data = productList
+        });
+    }
+
+    [HttpGet("byId")]
+    public async Task<IActionResult> GetProductsById(Guid id)
+    {
+        var productToFetch = await _product.GetProductsById(id);
+
+        return Ok(new Response<ProductDTO>
+        {
+            Success = true,
+            Message = "Product by ID fetched",
+            Data = productToFetch
+        });
+    }
+
+    [HttpPost("add")]
+    public async Task<IActionResult> AddProducts(AddProductsDTO addProductsDTO)
+    {
+        await _product.AddIngredients(addProductsDTO);
+
+        return Ok(new Response<AddProductsDTO>
+        {
+            Success = true,
+            Message = "Products have been added",
+            Data = addProductsDTO
+        });
+    }
+
+    [HttpPatch("edit")]
+    public async Task<IActionResult> EditProducts(EditProductDTO editProductDTO, Guid productID)
+    {
+        await _product.EditIngredients(editProductDTO, productID);
+
+        return Ok(new Response<EditProductDTO>
+        {
+            Success = true,
+            Message = "Product edited",
+            Data = editProductDTO
+        });
+    }
+
+    [HttpDelete("remove")]
+    public async Task<IActionResult> RemoveProducts(Guid id)
+    {
+        var productToRemove = await _product.GetProductsById(id);
+        await _product.RemoveIngredients(id);
+
+        return Ok(new Response<ProductDTO>
+        {
+            Success = true,
+            Message = "Product Removed",
+            Data = productToRemove
+        });
+    }
+}
