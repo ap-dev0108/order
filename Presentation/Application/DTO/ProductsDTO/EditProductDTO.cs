0f28b1f33d346759c5f76df471e2e02463d6cea7
Aryan <ap01n08@gmail.com>
2026-08-20 18:30:47 +0545

    refactor(dto): Removing AddProductsDTO from the file and extracting necessary columns

--- Presentation/Application/DTO/ProductsDTO/EditProductDTO.cs
+++ Presentation/Application/DTO/ProductsDTO/EditProductDTO.cs
@@ -1,8 +1,13 @@
+using OrderManagement.Domain.Enum;
+
 namespace OrderManagement.Application.DTO.Products;
 
 public class EditProductDTO
 {
-    public AddProductsDTO addProductsDTO {get; set;}
-    public DateTime LastUpdated {get; set;} = DateTime.UtcNow;
-    public string Notes {get; set;} = string.Empty;
+    public string Title {get; set;} = string.Empty;
+    public Unit Units {get; set;}
+    public decimal QualityOnHand {get; set;}
+    public decimal ReorderThreshold {get; set;}
+    public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
+    public string Notes { get; set; } = string.Empty;
 }
