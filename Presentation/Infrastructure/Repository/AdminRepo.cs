532cb72ab3b890c375c13707ea8d83d34ca373b8
Aryan <ap01n08@gmail.com>
2026-08-20 13:43:37 +0545

    chore(repo): Adding queries for getting user via id

--- Presentation/Infrastructure/Repository/AdminRepo.cs
+++ Presentation/Infrastructure/Repository/AdminRepo.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using OrderManagement.Application.Identity;
 using OrderManagement.Application.Interface;
@@ -8,14 +9,21 @@ namespace OrderManagement.Infrastructure.Repo;
 public class AdminRepo : IAdminRepo
 {
     private readonly AppDbContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public AdminRepo(AppDbContext db)
+    public AdminRepo(AppDbContext db, UserManager<ApplicationUser> userManager)
     {
         _db = db;
+        _userManager = userManager;
     }
 
     public async Task<List<ApplicationUser>> GetAllUsers()
     {
         return await _db.Users.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<ApplicationUser> GetUserById(string id)
+    {
+        return await _db.Users.AsNoTracking().FirstOrDefaultAsync(f => f.Id == id);
     }
 }
