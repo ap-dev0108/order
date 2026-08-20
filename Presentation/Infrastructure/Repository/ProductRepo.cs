a4d9a0d6bedfd3ed043803217f140d3b8a2e740d
Aryan <ap01n08@gmail.com>
2026-08-20 14:43:24 +0545

    chore(repo): Creating repo for product related queries

---
+++ Presentation/Infrastructure/Repository/ProductRepo.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using OrderManagement.Application.Interface;
+using OrderManagement.Domain.Entities;
+using OrderManagement.Infrastructure.Persistence;
+
+namespace OrderManagement.Infrastructure.Repo;
+
+public class ProductRepo : IProductRepo
+{
+    private readonly AppDbContext _db;
+
+    public ProductRepo(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<List<Ingredient>> GetIngredientsAsync()
+    {
+        return await _db.Ingredients.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<Ingredient> GetIngredientById(Guid id)
+    {
+        return await _db.Ingredients.AsNoTracking().FirstOrDefaultAsync(f => f.Id == id);
+    }
+
+    public async Task AddIngredients(Ingredient ingredient)
+    {
+        _db.Ingredients.Add(ingredient);
+    }
+
+    public async Task RemoveIngredients(Ingredient ingredient)
+    {
+        _db.Ingredients.Remove(ingredient);
+    }
+}
