77ae71bd798c479566d3a7d6ba2e67b200512b69
Aryan <ap01n08@gmail.com>
2026-08-20 13:44:54 +0545

    feat(services): Implementing logics for getting user by id

--- Presentation/Application/Services/AdminServices.cs
+++ Presentation/Application/Services/AdminServices.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Identity;
 using OrderManagement.Application.DTO;
+using OrderManagement.Application.Identity;
 using OrderManagement.Application.Interface;
 
 namespace OrderManagement.Application.Services;
@@ -6,10 +8,12 @@ namespace OrderManagement.Application.Services;
 public class AdminServices
 {
     private readonly IAdminRepo _admin;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public AdminServices(IAdminRepo admin)
+    public AdminServices(IAdminRepo admin, UserManager<ApplicationUser> userManager)
     {
         _admin = admin;
+        _userManager = userManager;
     }
 
     public async Task<List<UserDTO>> GetUsers()
@@ -18,9 +22,24 @@ public class AdminServices
 
         return userList.Select(s => new UserDTO
         {
+            Id = s.Id,
             Email = s.Email,
             isActive = s.IsActive,
             FullName = s.FullName
         }).ToList();
+    }
+
+    public async Task<UserDTO> GetUsersById(string id)
+    {
+        var user = await _userManager.FindByIdAsync(id) ??
+            throw new KeyNotFoundException("User with the ID cannot be found");
+
+        return new UserDTO
+        {
+            Id = user.Id,
+            FullName = user.FullName,
+            Email = user.Email,
+            isActive = user.IsActive
+        };
     }
 }
