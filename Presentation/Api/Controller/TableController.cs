b1968ef4a5ff3075c22e9cae5f2c5da359e596dc
Aryan <ap01n08@gmail.com>
2026-08-21 11:26:23 +0545

    feat(api): Creating api for the table controller

---
+++ Presentation/Api/Controller/TableController.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using OrderManagement.Application.DTO;
+using OrderManagement.Application.DTO.Table;
+using OrderManagement.Application.Services;
+
+namespace OrderManagement.Api.Controller;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TableController : ControllerBase
+{
+    private readonly TableServices _tableServices;
+
+    public TableController(TableServices tableServices)
+    {
+        _tableServices = tableServices;
+    }
+
+    [HttpGet("all")]
+    public async Task<IActionResult> GetAllTablesAsync()
+    {
+        var tableList = await _tableServices.GetRestaurantTablesAsync();
+
+        return Ok(new Response<List<TableDTO>>
+        {
+            Success = true,
+            Message = "Table List fetched",
+            Data = tableList
+        });
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetTableById(Guid id)
+    {
+        var table = await _tableServices.GetRestaurantTablesById(id);
+
+        return Ok(new Response<TableDTO>
+        {
+            Success = true,
+            Message = "Table fetched by ID",
+            Data = table
+        });
+    }
+
+    [HttpPost("add")]
+    public async Task<IActionResult> AddTables(AddTableDTO addTableDTO)
+    {
+        await _tableServices.AddRestaurantTables(addTableDTO);
+
+        return Ok(new Response<AddTableDTO>
+        {
+            Success = true,
+            Message = "Table added",
+            Data = addTableDTO
+        });
+    }
+
+    [HttpDelete("remove")]
+    public async Task<IActionResult> RemoveTables(Guid id)
+    {
+        var tableToRemove = await _tableServices.GetRestaurantTablesById(id);
+        await _tableServices.RemoveTables(id);
+
+        return Ok(new Response<TableDTO>
+        {
+            Success = true,
+            Message = "Table with the provided ID has been removed",
+            Data = tableToRemove
+        });
+    }
+}
