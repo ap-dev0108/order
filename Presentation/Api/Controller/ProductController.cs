7fbb9114b46a4c097c3427228bd74a3ae2adf0ed
Aryan <ap01n08@gmail.com>
2026-08-21 10:21:10 +0545

    refactor(product): Authorizing product apis with management role

--- Presentation/Api/Controller/ProductController.cs
+++ Presentation/Api/Controller/ProductController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OrderManagement.Application.DTO;
 using OrderManagement.Application.DTO.Products;
@@ -5,6 +6,9 @@ using OrderManagement.Application.Services;
 
 namespace OrderManagement.Api.Controller;
 
+[ApiController]
+[Route("api/[controller]")]
+[Authorize(Roles = "Management")]
 public class ProductController : ControllerBase
 {
     private readonly ProductServices _product;
