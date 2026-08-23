d0bb339b5a15082665b98fb429e7d21d2f95f883
Aryan <ap01n08@gmail.com>
2026-08-23 09:52:17 +0545

    chore(dto): Creating dtos for menu categories and items seperately

---
+++ Presentation/Application/DTO/MenuDTO/MenuCategoryDTO/CreateMenuCategory.cs
@@ -0,0 +1,6 @@
+namespace OrderManagement.Application.DTO.Menu.Category;
+
+public class CreateMenuCategory
+{
+    public string MenuCategoryTitle {get; set;} = string.Empty;
+}
---
+++ Presentation/Application/DTO/MenuDTO/MenuCategoryDTO/DisplayMenuCategory.cs
@@ -0,0 +1,7 @@
+namespace OrderManagement.Application.DTO.Menu.Category;
+
+public class DisplayMenuCategory
+{
+    public Guid Id {get; set;}
+    public string MenuCategoryTitle {get; set;} = string.Empty;
+}
