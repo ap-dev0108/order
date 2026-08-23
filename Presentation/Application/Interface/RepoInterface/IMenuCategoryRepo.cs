52cfd04b588287dd1ae9a6725a8e1630b3e06459
Aryan <ap01n08@gmail.com>
2026-08-23 10:19:57 +0545

    refactor(interface): Adding bool parameter for ascending and descending order for menu display

--- Presentation/Application/Interface/RepoInterface/IMenuCategoryRepo.cs
+++ Presentation/Application/Interface/RepoInterface/IMenuCategoryRepo.cs
@@ -4,7 +4,7 @@ namespace OrderManagement.Application.Interface;
 
 public interface IMenuCategoryRepo
 {
-    Task<List<MenuCategory>> GetMenuCategoriesAsync();
+    Task<List<MenuCategory>> GetMenuCategoriesAsync(bool ascending = true);
     Task<MenuCategory> MenuCategoryByIdAsync(Guid id);
 
     Task AddMenuCategory(MenuCategory menuCategory);
