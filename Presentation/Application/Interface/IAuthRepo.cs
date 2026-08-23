be3cc77d2cb898a7192a8ed33ed7fd4bf3949f79
Aryan <ap01n08@gmail.com>
2026-08-23 12:29:47 +0545

    global commit

--- Presentation/Application/Interface/IAuthRepo.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-using OrderManagement.Application.Identity;
-
-namespace OrderManagement.Application.Interface;
-
-public interface IAuthRepo
-{
-    Task<SignInResult> LoginUser(ApplicationUser user, string passowrd);
-}
