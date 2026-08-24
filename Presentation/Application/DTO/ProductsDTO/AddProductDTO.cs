a070d2067b3075e8b05df1eb1d721941fda03a0a
Aryan <ap01n08@gmail.com>
2026-08-24 19:16:18 +0545

    chore(dto): Updating dto for inventory transactions too

--- Presentation/Application/DTO/ProductsDTO/AddProductDTO.cs
+++ Presentation/Application/DTO/ProductsDTO/AddProductDTO.cs
@@ -1,3 +1,4 @@
+using OrderManagement.Domain.Entities;
 using OrderManagement.Domain.Enum;
 
 namespace OrderManagement.Application.DTO.Products;
@@ -9,4 +10,14 @@ public class AddProductsDTO
     public decimal QualityOnHand {get; set;}
     public decimal ReorderThreshold {get; set;}
     public DateTime UpdatedAt {get; set;} = DateTime.UtcNow;
+}
+
+public class AddInventoryTransaction
+{
+    public Guid IngredientId {get; set;}
+    public Ingredient Ingredients {get; set;}
+
+    public decimal ChargeAmount {get; set;}
+
+    public DateTime CreatedAt {get; set;} = DateTime.UtcNow;
 }
--- Presentation/Application/DTO/ProductsDTO/EditProductDTO.cs
+++ Presentation/Application/DTO/ProductsDTO/EditProductDTO.cs
@@ -1,3 +1,4 @@
+using OrderManagement.Domain.Entities;
 using OrderManagement.Domain.Enum;
 
 namespace OrderManagement.Application.DTO.Products;
@@ -10,4 +11,14 @@ public class EditProductDTO
     public decimal ReorderThreshold {get; set;}
     public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
     public string Notes { get; set; } = string.Empty;
+}
+
+public class EditInventoryTransaction
+{
+    public Guid IngredientId {get; set;}
+    public Ingredient Ingredients {get; set;}
+
+    public decimal ChargeAmount {get; set;}
+
+    public DateTime CreatedAt {get; set;} = DateTime.UtcNow;
 }
