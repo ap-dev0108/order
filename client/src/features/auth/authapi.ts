a55d01998c05b72ac7e485f6888689fafe41c3ee
Aryan <ap01n08@gmail.com>
2026-08-30 12:51:37 +0545

    config(api): Creating function for posting data on the login url

--- client/src/features/auth/authapi.ts
+++ client/src/features/auth/authapi.ts
@@ -2,6 +2,6 @@ import api from "../../services/api/api";
 import type { LoginData } from "../../types/auth/AuthType";
 
 export const Login = async(data: LoginData) : Promise<LoginData> => {
-    const response = await api.post<LoginData>("/auth/login", data);
+    const response = await api.post<LoginData>("/login", data);
     return response.data;
 }
