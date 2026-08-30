267e6cec8b89d7d840d5574b667249a3a3793660
Aryan <ap01n08@gmail.com>
2026-08-30 21:16:14 +0545

    chore(navigation): Creating a dummy navigation for now

--- client/src/features/auth/hooks/useLogin.ts
+++ client/src/features/auth/hooks/useLogin.ts
@@ -1,14 +1,17 @@
 import { QueryClient, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Login } from "../authapi";
+import { useNavigate } from "react-router-dom";
 
 export const useLogin = () => {
     const query = useQueryClient();
+    const navigate = useNavigate();
 
     return useMutation({
         mutationKey: ["login"],
         mutationFn: Login,
         
         onSuccess: () => {
+            navigate("/admin");
             query.invalidateQueries({ queryKey: ["login"] });
         },
 
