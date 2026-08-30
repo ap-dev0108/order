5c468fe95f68377e5757b6fbd9e5fa75032284b0
Aryan <ap01n08@gmail.com>
2026-08-30 13:51:24 +0545

    refactor(routes): Switching the routing from app to this file

---
+++ client/src/routes/AppRoutes.tsx
@@ -0,0 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import AuthLayout from "../layout/AuthLayout";
+
+import { LoginFormPage } from "../pages/Login";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      {/* Application routes */}
+      {/* <Route element={<MainLayout />}>
+                <Route path="/" element={<HomePage />} />
+            </Route> */}
+
+      {/* Authentication routes */}
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<LoginFormPage />} />
+      </Route>
+
+      {/* Catch-all */}
+      {/* <Route path="*" element={<NotFoundPage />} /> */}
+    </Routes>
+  );
+}
+
+export default AppRoutes;
