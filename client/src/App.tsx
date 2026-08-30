5de597f4f657d39da38edcabded4061f26142225
Aryan <ap01n08@gmail.com>
2026-08-30 13:53:04 +0545

    refactor(app): Importing app routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,7 @@
-import { Routes, Route } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
 
-import MainLayout from "./layout/MainLayout";
-import AuthLayout from "./layout/AuthLayout";
-
-import { LoginFormPage } from "./pages/Login";
-
-function AppRoutes() {
-  return (
-    <Routes>
-      {/* Application routes */}
-      {/* <Route element={<MainLayout />}>
-                <Route path="/" element={<HomePage />} />
-            </Route> */}
-
-      {/* Authentication routes */}
-      <Route element={<AuthLayout />}>
-        <Route path="/login" element={<LoginFormPage />} />
-      </Route>
-
-      {/* Catch-all */}
-      {/* <Route path="*" element={<NotFoundPage />} /> */}
-    </Routes>
-  );
+function App() {
+  return <AppRoutes />
 }
 
-export default AppRoutes;
+export default App;
