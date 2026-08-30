2ae146b71edc3d409c59a534572f40d2879a404c
Aryan <ap01n08@gmail.com>
2026-08-30 21:12:21 +0545

    chore(routes): Adding admin

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -2,14 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import AuthLayout from "../layout/AuthLayout";
 
 import { LoginFormPage } from "../pages/Login";
+import MainLayout from "../layout/MainLayout";
+import { Admin } from "../pages/Admin";
 
 function AppRoutes() {
   return (
     <Routes>
       {/* Application routes */}
-      {/* <Route element={<MainLayout />}>
-                <Route path="/" element={<HomePage />} />
-            </Route> */}
+      <Route element={<MainLayout />}>
+                <Route path="/admin" element={<Admin />} />
+            </Route>
 
       {/* Authentication routes */}
       <Route element={<AuthLayout />}>
