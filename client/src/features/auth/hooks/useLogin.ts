e1803093be836a825dc74ec227afdff821a7516e
Aryan <ap01n08@gmail.com>
2026-08-30 14:09:45 +0545

    config(login): Injecting validating queries

--- client/src/features/auth/hooks/useLogin.ts
+++ client/src/features/auth/hooks/useLogin.ts
@@ -1,9 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { QueryClient, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Login } from "../authapi";
 
 export const useLogin = () => {
+    const query = useQueryClient();
+
     return useMutation({
         mutationKey: ["login"],
-        mutationFn: Login
+        mutationFn: Login,
+        
+        onSuccess: () => {
+            query.invalidateQueries({ queryKey: ["login"] });
+        },
+
     })
 }
