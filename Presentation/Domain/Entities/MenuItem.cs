1c614a9e4bb266034de8c297db8de81269cab918
Aryan <ap01n08@gmail.com>
2026-08-30 10:06:51 +0545

    update(entity): Updating column datatype Category from collection to MenuCategory

--- Presentation/Domain/Entities/MenuItem.cs
+++ Presentation/Domain/Entities/MenuItem.cs
@@ -5,19 +5,17 @@ namespace OrderManagement.Domain.Entities;
 public class MenuItem
 {
     [Key]
-    public Guid Id {get; set;} = Guid.NewGuid();
+    public Guid Id { get; set; } = Guid.NewGuid();
 
-    public Guid CategoryId {get; set;}
-    public ICollection<MenuCategory> Category {get; set;}
+    public MenuCategory Category { get; set; }
 
-    public Guid IngredientId {get; set;}
-    public ICollection<Ingredient> Ingredients {get; set;}
+    public ICollection<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
 
-    public string MenuItemTitle {get; set;} = string.Empty;
-    public string? MenuItemDescription {get; set;}
+    public string MenuItemTitle { get; set; } = string.Empty;
+    public string? MenuItemDescription { get; set; }
 
-    public decimal MenuItemPrice {get; set;}
+    public decimal MenuItemPrice { get; set; }
 
-    public bool IsAvailable {get; set;}
-    public string ImageUrl {get; set;} = string.Empty;
+    public bool IsAvailable { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
 }
