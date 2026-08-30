d752bef0606ff4d240369fb8905c0c12905c1590
Aryan <ap01n08@gmail.com>
2026-08-30 10:11:00 +0545

    chore(table dto): Updating the display table dto

--- Presentation/Application/DTO/TableDTO/TableDTO.cs
+++ Presentation/Application/DTO/TableDTO/TableDTO.cs
@@ -1,14 +1,18 @@
+using OrderManagement.Domain.Entities;
+
 namespace OrderManagement.Application.DTO.Table;
 
 public class TableDTO
 {
-    public Guid Id {get; set;}
+    public Guid Id { get; set; } = Guid.NewGuid();
 
-    public int TableNumber {get; set;}
+    public int TableNumber { get; set; }
 
-    public string QrCodeToken {get; set;} = string.Empty;
+    public string QrCodeToken { get; set; } = string.Empty;
 
-    public int Capacity {get; set;}
+    public int Capacity { get; set; }
 
-    public bool IsActive {get; set;}
+    public bool IsActive { get; set; }
+
+    public ICollection<DinningSession> Sessions { get; set; }
 }
