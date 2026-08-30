6e62f5d86d378b90badc79020c98d5fd8c5b825c
Aryan <ap01n08@gmail.com>
2026-08-30 10:16:23 +0545

    config(dependency): Injecting the table services

--- Presentation/Infrastructure/DI/ServicesDI.cs
+++ Presentation/Infrastructure/DI/ServicesDI.cs
@@ -14,6 +14,7 @@ public class ServiceDI
         services.AddScoped<DataHelper>();
         services.AddScoped<ProductServices>();
         services.AddScoped<DinningServices>();
+        services.AddScoped<TableServices>();
         services.AddScoped<MenuCategoryService>();
         services.AddScoped<MenuItemServices>();
         services.AddScoped<OrderServices>();
