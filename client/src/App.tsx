23b705f75ba559983fd579336b7fe3d3e906ab35
Aryan <ap01n08@gmail.com>
2026-08-30 13:46:38 +0545

    refactor(app): Removing the default codes, and replacing with routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,27 @@
-import { useState } from "react";
-import heroImg from "./assets/hero.png";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0);
+import MainLayout from "./layout/MainLayout";
+import AuthLayout from "./layout/AuthLayout";
 
+import { LoginFormPage } from "./pages/Login";
+
+function AppRoutes() {
   return (
-    <>
-      <span> Hi </span>
-    </>
+    <Routes>
+      {/* Application routes */}
+      {/* <Route element={<MainLayout />}>
+                <Route path="/" element={<HomePage />} />
+            </Route> */}
+
+      {/* Authentication routes */}
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<LoginFormPage />} />
+      </Route>
+
+      {/* Catch-all */}
+      {/* <Route path="*" element={<NotFoundPage />} /> */}
+    </Routes>
   );
 }
 
-export default App;
+export default AppRoutes;
