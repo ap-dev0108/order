1532bf459c3306c28aae8c1687e8bbfa725cc441
Aryan <ap01n08@gmail.com>
2026-08-30 10:11:41 +0545

    feat(session): Updating interface for checking active session methods

--- Presentation/Application/Interface/RepoInterface/IDinningRepo.cs
+++ Presentation/Application/Interface/RepoInterface/IDinningRepo.cs
@@ -6,5 +6,6 @@ public interface IDinningRepo
 {
     Task<List<DinningSession>> GetDinningSessionsAsync();
     Task<DinningSession> GetDinningSessionById(Guid id);
+    Task<bool> HasActiveSession(Guid tableId);
     Task AddDinning(DinningSession dinningSession);
 }
